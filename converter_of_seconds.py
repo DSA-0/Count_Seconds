b5030b1118d18da4f2704061f0332649a1f2f0aa
DSA <123333421+DSA-0@users.noreply.github.com>
2023-03-22 16:56:56 -0300

    Update converter_of_seconds.py

--- converter_of_seconds.py
+++ converter_of_seconds.py
@@ -3,33 +3,28 @@
 
 magnums = [[' century(s), ', 3155760000], [' decade(s), ', 315352800], [' year(s), ', 31536000],
            [' month(s), ', 2626560], [' week(s), ', 604800], [' day(s), ', 86400], [' hour(s), ', 3600],
-           [' minute(s) and ', 60], [' second(s).']]  # ->List of time magnitude and yours respective seconds.
-
-response = ''  # -> Here is the answer that will be constructed as per the calculations.
+           [' minute(s) and ', 60], [' second(s).']]  # -> List of time magnitude and yours respective seconds.
 
 
-def t_magnum(num, i_list0, i_list1):  # -> Function that calculates the magnitudes and constructs the answer.
-    global response
-    magnum = num // i_list1
-    rest = num % i_list1
-    if magnum > 0:
-        response += str(magnum) + i_list0
-    return rest
-
-
-def multi_magnum(num):  # -> Function that structures previous function calls and outputs the response. 
-    global response
-    n = t_magnum(num, magnums[0][0], magnums[0][1])
-    for i in magnums[1:8]:
-        n = t_magnum(n, i[0], i[1])
-    response += str(n) + magnums[8][0]
+def total_mgm(num, list1):  # -> Function that calculates the magnitudes and constructs the answer.
+    response = ''  # -> Here's the answer that will be constructed as per the calculations.
+    mgm = num // list1[0][1]  # -> First calculation and your output, if there's.
+    after = num % list1[0][1]
+    if mgm > 0:
+        response += str(mgm) + list1[0][1]
+    for i in list1[1:8]:  # -> Others calculations and yours outputs, except the last.
+        mgm = after // i[1]
+        after = after % i[1]
+        if mgm > 0:
+            response += str(mgm) + i[0]
+    response += str(after) + list1[8][0]  # -> Last output.
     print(f'Your enter corresponds to...\n{response}')
 
 
 def main():
     try:
         start = int(input('Please, enter with integer number for conversion: '))
-        multi_magnum(start)
+        total_mgm(start, magnums)
     except ValueError:  # -> Common error from entry.
         print('Could not run the code correctly. Did you enter an integer?')
     except Exception as error:  # -> Any others errors.
